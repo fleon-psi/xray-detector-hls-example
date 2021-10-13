bf0dccd9dd772e5140779dd61ff84410021452b2
Filip Leonarski <filip.leonarski@psi.ch>
2021-10-13 16:09:17 +0200

    Improved example

--- conversion.cpp
+++ conversion.cpp
@@ -1,39 +1,26 @@
 #include "conversion.h"
 
+// Rounding of fraction to closest integer (standard truncation will introduce error)
 template <class T>
 ap_int<16> conv_round (T val) {
+#pragma HLS INLINE
 	if (val >= 0)
 		return (ap_int<16>)(val + static_cast<T>(0.5));
 	else return (ap_int<16>)(val - static_cast<T>(0.5));
 }
 
-ap_int<16> xray_convert(ap_uint<16> input,
-		type_g0 g0, type_g1 g1, type_g2 g2,
-		type_p  p0, type_p  p1, type_p  p2) {
-
-	// For a 16-bit pixel read-out:
-	//    -  2 bits for mode of the detector (00: mode 0, 01: mode 1, 11: mode 2, 10: impossible)
-	//    - 14 bits for accumulated charge, that need subtraction of constant p and division by constant g
-
-	// There are 6 conversion constants:
-	//    - Constants p0,p1,p2 are always in range 0 - 16384
-	//    - Constant g0 is always in range 0.0313 - 0.0625
-	//    - Constant g1 is always in range 0.2500 - 0.5000
-	//    - Constant g2 is always in range 1.0000 - 2.0000
-
-	ap_uint<2> mode = input(15,14);
-	type_p     adu = input(13,0);
-	switch (mode) {
-	case 0:
-		return conv_round((adu - p0) * g0);
-	case 1:
-		return conv_round((adu - p1) * g1);
-	case 3:
-		return conv_round((adu - p2) * g2);
-	default:
-		return -32676; // just indication that wrong value is placed
-	}
+// Copy bit-to-bit int64 to double (no conversion)
+// This trick is often necessary in HLS, if one reads word as ap_uint<512>, and wants to read individual bytes
+double bit_int2double(uint64_t input) {
+#pragma HLS INLINE
+	union fp_int_union {
+		uint64_t int_val;
+		double fp_val;
+	} x;
+	x.int_val = input;
+	return x.fp_val;
 }
+
 
 // These functions pack/unpack any 16-bit arbitrary precision format to 512-bit
 template <class T>
@@ -50,28 +37,51 @@ void pack(ap_uint<512> &output, T input[32]) {
 		output[i] = input[i / 16][i % 16];
 }
 
-void convert_packet(ap_uint<512> *host_mem_in, ap_uint<512> *host_mem_out,
-					type_g0 *g0, type_g1 *g1, type_g2 *g2,
-					type_p *p0, type_p *p1, type_p *p2,
+// Functions to load constants from memory
+void load(ap_uint<512> *input, double *out) {
+	for (int i = 0; i < N / 8; i++) {
+		ap_uint<512> tmp = input[i];
+		for (int j = 0; j < 8; j++) {
+			out[i * 8 + j] = bit_int2double(tmp(j * 64 + 63, j * 64));
+		}
+	}
+}
+
+template <class T>
+void load(ap_uint<512> *input, T *out) {
+	for (int i = 0; i < N /32 ; i++)
+		unpack(input[i], out + i * 32);
+}
+
+void convert(ap_uint<512> *pixels_in, ap_uint<512> *pixels_out, type_p0 *p0, type_g0 *g0, uint64_t npackets) {
+	for (int i = 0; i < npackets ; i++) {
+//#pragma HLS PIPELINE II=1
+			ap_uint<16> input[32], output[32];
+			// One packet is 512-bits, so 32 x 16-bit numbers
+			unpack(pixels_in[i], input);
+			for (int j = 0; j < 32; j++) {
+				output[j] = conv_round((input[j] - p0[i * 32 + j]) * g0[i * 32 + j]);
+			}
+			pack(pixels_out[i], output);
+		}
+}
+
+void convert_packet(ap_uint<512> *host_mem_in,
+					ap_uint<512> *host_mem_out,
 					action_reg *act) {
 #pragma HLS INTERFACE s_axilite port=act offset=0x80
 #pragma HLS INTERFACE m_axi depth=512 port=host_mem_in  offset=off
 #pragma HLS INTERFACE m_axi depth=512 port=host_mem_out offset=off
-#pragma HLS INTERFACE m_axi depth=512 port=p0 offset=off
-#pragma HLS INTERFACE m_axi depth=512 port=p1 offset=off
-#pragma HLS INTERFACE m_axi depth=512 port=p2 offset=off
-#pragma HLS INTERFACE m_axi depth=512 port=g2 offset=off
-#pragma HLS INTERFACE m_axi depth=512 port=g0 offset=off
-#pragma HLS INTERFACE m_axi depth=512 port=g1 offset=off
 
-	for (int i = 0; i < act->npackets; i++) {
-		ap_uint<16> input[32], output[32];
-		unpack(host_mem_in[act->offset_in  / 64 + i], input);
+	type_p0 p0[N];
+#pragma HLS RESOURCE variable=p0 CORE=RAM_2P_URAM
+//#pragma HLS ARRAY_PARTITION variable=p0 cyclic factor=32
+	type_g0 g0[N];
+#pragma HLS RESOURCE variable=g0 CORE=RAM_2P_URAM
+//#pragma HLS ARRAY_PARTITION variable=g0 cyclic factor=32
 
-		for (int j = 0; j < 32; j++)
-			output[j] = xray_convert(input[j], g0[i * 32 + j], g1[i * 32 + j], g2[i * 32 + j],
-									 p0[i * 32 + j], p1[i * 32 + j], p2[i * 32 + j]);
+	load(host_mem_in + act->offset_p0 / 64, p0);
+	load(host_mem_in + act->offset_g0 / 64, g0);
 
-		pack(host_mem_out[act->offset_out / 64 + i], output);
-	}
+	convert(host_mem_in + act->offset_in / 64, host_mem_out + act->offset_out / 64, p0, g0, act->npackets);
 }
--- conversion.h
+++ conversion.h
@@ -2,22 +2,21 @@
 #include <hls_stream.h>
 #include <ap_int.h>
 
-#define type_p  double
-#define type_g0 double
-#define type_g1 double
-#define type_g2 double
+typedef double type_p0;
+typedef double type_g0;
+
+//typedef ap_ufixed<16, 14, AP_RND_CONV> type_p0;
+//typedef ap_ufixed<16,  1, AP_RND_CONV> type_g0;
+
+// for tests need a relatively small number here, so it doesn't cause issues with stack size on CPU
+#define N (256*1024)
 
 struct action_reg {
 	uint64_t offset_in;
 	uint64_t offset_out;
+	uint64_t offset_p0;
+	uint64_t offset_g0;
 	uint64_t npackets;
 };
 
-ap_int<16> xray_convert(ap_uint<16> input,
-		type_g0 g0, type_g1 g1, type_g2 g2,
-		type_p  p0, type_p  p1, type_p  p2);
-
-void convert_packet(ap_uint<512> *host_mem_in, ap_uint<512> *host_mem_out,
-					type_g0 *g0, type_g1 *g1, type_g2 *g2,
-					type_p *p0, type_p *p1, type_p *p2,
-					action_reg *act);
+void convert_packet(ap_uint<512> *host_mem_in, ap_uint<512> *host_mem_out, action_reg *act);
